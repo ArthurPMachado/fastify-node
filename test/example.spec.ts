4a36ca37ae87744a8235e5c25cd23b49a9e12c00
Arthur Machado <arthur.machado@iclubs.tech>
2023-08-18 00:10:27 -0300

    add test to create a new transaction

---
+++ test/example.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+
+import { app } from '../src/app'
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('The user should be able to create a new transaction', async () => {
+  const response = await request(app.server).post('/transactions').send({
+    title: 'New transaction',
+    amount: 5000,
+    type: 'credit',
+  })
+
+  expect(response.status).toEqual(201)
+})
